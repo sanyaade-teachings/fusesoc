f32991d9f3c82facef2f021de99ed33b45adefd4
Olof Kindgren <olof.kindgren@gmail.com>
2024-12-19 13:14:16 +0100

    wip: libdeps

--- fusesoc/filters/splitlib.py
+++ fusesoc/filters/splitlib.py
@@ -4,11 +4,20 @@ import os
 logger = logging.getLogger(__name__)
 
 
+def flatten_vlnv(vlnv):
+    return str(vlnv).lstrip(":").replace(":", "_").replace(".", "_")
+
+
 class Splitlib:
     def run(self, edam, work_root):
+        libdeps = {}
+        for k, v in edam["dependencies"].items():
+            libdeps[flatten_vlnv(k)] = [flatten_vlnv(x) for x in v]
+
+        libdeps.update(edam["flow_options"].get("library_dependencies", {}))
+        # edam["flow_options"]["library_dependencies"] = libdeps
+        edam["library_dependencies"] = libdeps
         for f in edam["files"]:
             if not "logical_name" in f:
-                f["logical_name"] = (
-                    str(f["core"]).lstrip(":").replace(":", "_").replace(".", "_")
-                )
+                f["logical_name"] = flatten_vlnv(f["core"])
         return edam
